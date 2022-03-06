e02c23c5129ade3dc303f09b32edf2c693e8e997
Hilmi Laptop <57535737+codebyfrosty@users.noreply.github.com>
2022-03-07 04:01:06 +0700

    first bkd

---
+++ lib/halUtama.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Utama extends StatelessWidget {
+  // const Utama({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text('Modul'),
+      ),
+      
+    );
+  }
+}
